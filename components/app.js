db77f4206ee4bf833e1a05909dc23e831024c0fb
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-04 16:43:01 +0545

    5.3 Fragements and Mapping Data to JSX

--- components/app.js
+++ components/app.js
@@ -1,14 +1,15 @@
 import Banner from "./banner";
+import HouseList from "./houseList";
 
 const App = () => {
   const jsx = <div>Hello React and Next js</div>
   return (
-    <Banner>
-      <div>
-        {jsx}
-        Providing houses all over the <span>world</span>
-      </div>
-    </Banner>
+    <>
+      <Banner>
+        <p> Providing houses all over the <span>world</span></p>
+      </Banner>
+      <HouseList />
+    </>
   )
 }
 
